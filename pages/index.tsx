1f8cbb4a435830f3d2541d2774c7e9b106b0cf2d
Surya-the-coder <naansuryayadav@gmail.com>
2022-08-29 23:30:00 +0530

    Changed style

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,19 +14,19 @@ const IndexPage = () => {
   return (
     <div className={LightMode ? 'flex flex-col w-full min-h-screen bg-white' : 'flex flex-col w-full min-h-screen bg-black'}>
       <NavBar LightMode = {LightMode} setLightMode = {setLightMode} />
-      <div className='flex pl-[35rem] pr-[8rem] pt-[23rem] absolute z-30'>
+      <div className='flex pl-96 pr-[8rem] pt-[23rem] absolute z-30'>
         <p className={`flex justify-center text-4xl w-full ${LightMode ? 'text-gray-500' : 'text-gray-400'}`}>
         Software Developer with over 3 years of experience in development of software products and good at problem-solving and software design. A personable individual who is also dynamic and adept at a variety of programing languages and frameworks. Always looking to decipher problems to arrive at a simple yet scalable solutions.
         </p>
       </div>
       <div className=' flex flex-col pt-[24rem] pl-[12rem] relative z-0'>
         <div className='flex justify-left'>
-          <p className={` flex justify-center text-[96px] mx-20 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 `}>
+          <p className={'flex justify-center text-[96px] mx-20 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '}>
             Surya
           </p>
         </div>
         <div className='flex justify-left'>
-          <p className={`flex justify-center text-[96px] mx-20 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500`}>
+          <p className={'flex justify-center text-[96px] mx-20 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'}>
             Sundararajan
           </p>
           {/* <h1 className = "font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"> Tailwind CSS </h1> */}
